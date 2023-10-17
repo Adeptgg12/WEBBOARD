4706ebe62e29f850aec96b8a0b7787f12c728286
Adeptgg12 <102339347+Adeptgg12@users.noreply.github.com>
2023-10-17 21:35:34 +0700

    update signup username already exit

--- register.php
+++ register.php
@@ -1,11 +1,11 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // เชื่อมต่อกับฐานข้อมูล (แทนค่าด้วยข้อมูลของคุณ)
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "mydatabase1";
-	$conn = mysqli_connect($servername, $username, $password, $dbname);
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "mydatabase1";
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
@@ -16,14 +16,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["txtPassword"];
     $name = $_POST["txtName"];
 
-    // นำข้อมูลไปเพิ่มลงในฐานข้อมูล (เช่นตาราง users)
-    $sql = "INSERT INTO member (name,username, password) VALUES ('$name','$username', '$password')";
+    // ตรวจสอบว่า username ซ้ำหรือไม่
+    $check_query = "SELECT * FROM member WHERE username = '$username'";
+    $check_result = $conn->query($check_query);
 
-    if ($conn->query($sql) === TRUE) {
-        header("location: index.html");
-        echo "User registered successfully";
+    if ($check_result->num_rows > 0) {
+        header("location: signup.php");
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+        // ถ้า username ไม่ซ้ำกัน
+        // นำข้อมูลไปเพิ่มลงในฐานข้อมูล (เช่นตาราง member)
+        $sql = "INSERT INTO member (name, username, password) VALUES ('$name', '$username', '$password')";
+
+        if ($conn->query($sql) === TRUE) {
+            header("location: index.html");
+            echo "User registered successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
     }
 
     $conn->close();
